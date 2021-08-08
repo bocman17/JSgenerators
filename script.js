2f00000029cc7a909ce6164f0e62f3570b91eada
bocman17 <hejlikt@gmail.com>
2021-08-08 10:47:49 +0200

    generatorJS

---
+++ script.js
@@ -0,0 +1,12 @@
+function* generator(array) {
+  for(let i = 0; i < array.length; i++) {
+      yield array[i]
+  }
+    
+}
+
+
+const generatorObject = generator([1, 3, 5])
+console.log(generatorObject.next())
+console.log(generatorObject.next())
+
